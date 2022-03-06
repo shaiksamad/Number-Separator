fff67f4b061b7ed13c831ea35668a2e640ea48e8
Samad <41123191+shaiksamad@users.noreply.github.com>
2022-03-06 22:16:40 +0530

    added num-separator.js

---
+++ num-separator.js
@@ -0,0 +1,12 @@
+function separator(num, sep=",") {
+    num = num.toString()
+    if (num.length <= 3) return num
+    final = sep+num.slice(-3)
+    num = num.slice(0, -3)
+    while (num.length > 2) {
+        final = sep+num.slice(-2) + final
+        num = num.slice(0, -2)
+    }
+    final = num + final
+    return final
+}
